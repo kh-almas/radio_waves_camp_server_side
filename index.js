3f9675d91931bd36aa7cc7ecbb01f0d7b12058cf
kh-almas <services.almas@gmail.com>
2023-06-14 21:41:31 +0600

    order from cart

--- index.js
+++ index.js
@@ -56,6 +56,7 @@ async function run() {
         const usersCollection = client.db("RadioWavesCamp").collection("users");
         const classCollection = client.db("RadioWavesCamp").collection("class");
         const cartCollection = client.db("RadioWavesCamp").collection("cart");
+        const enrollCollection = client.db("RadioWavesCamp").collection("enroll");
 
         // logged in user data
         app.get('/loged-in-data/:email', async (req, res) => {
@@ -80,7 +81,7 @@ async function run() {
             res.send(result);
         })
 
-        //cart data
+        //insert cart data
         app.post('/cart', async (req, res) => {
             const data = req.body;
             const update = {
@@ -92,6 +93,83 @@ async function run() {
             const options = { upsert: true };
             const result = await cartCollection.updateOne(data, update, options);
             res.send(result);
+        })
+
+        // get cart data
+        app.get('/cart-item/:email', verifyJWT , async (req, res) => {
+            const TokenData = req.decoded.email;
+            const urlParams = req.params.email;
+            if(TokenData === urlParams){
+                const result = await cartCollection.find({studentEmail: urlParams}).toArray();
+                res.send(result);
+            }else{
+                return res.status(401).send({error: true, message: "unauthorized access"});
+            }
+
+        })
+
+        // enrollCollection data
+        app.post('/enrolled/:email', verifyJWT , async (req, res) => {
+            const data = req.body;
+            const TokenData = req.decoded.email;
+            const urlParams = req.params.email;
+            if(TokenData !== urlParams){
+                return res.status(401).send({error: true, message: "unauthorized access"});
+            }
+
+            const result = await enrollCollection.insertMany(data);
+            // for popular instructor
+            if(result?.insertedCount > 0){
+                data.map( async singleData => {
+                    const query = {email: singleData.instructorEmail};
+                    const result = await usersCollection.findOne(query);
+                    const InstructorTotalEnrolled = result.enroll || 0;
+                    const count = InstructorTotalEnrolled + 1;
+                    const newValues = {
+                        $set: {
+                            enroll: count,
+                        }
+                    };
+                    const insResult = await usersCollection.updateOne(query, newValues);
+                })
+            }
+
+            // for popular class
+            if(result?.insertedCount > 0){
+                data.map( async singleData => {
+                    const query = {_id: new ObjectId(singleData?.classId)};
+                    const classResult = await classCollection.findOne(query);
+                    const InstructorTotalEnrolled = classResult.enroll || 0;
+                    const count = InstructorTotalEnrolled + 1;
+                    const newValues = {
+                        $set: {
+                            enroll: count,
+                        }
+                    };
+                    const insResult = await classCollection.updateOne(query, newValues);
+
+                    const cartQuery = {_id: new ObjectId(singleData?.cartId)};
+                    const result = await cartCollection.deleteOne(cartQuery);
+                })
+            }
+            res.send(result);
+
+        })
+
+        // delete cart data
+        app.delete('/remove-cart-item/:id/:email', verifyJWT , async (req, res) => {
+            console.log();
+            const TokenData = req.decoded.email;
+            const urlParams = req.params.email;
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            if(TokenData === urlParams){
+                const result = await cartCollection.deleteOne(query);
+                res.send(result);
+            }else{
+                return res.status(407).send({error: true, message: "unauthorized access"});
+            }
+
         })
 
         // For instructor
